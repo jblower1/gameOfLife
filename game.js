7099d72098d395489ef955feff10ae35a1b20b1c
John Murray <jbm1991@gmail.com>
2021-05-08 11:32:04 +0100

    bugfix: fix problems that were preventing it from running

--- game.js
+++ game.js
@@ -1,81 +1,104 @@
+const term = require("terminal-kit").terminal;
+
 let gameBoard = [];
 let gameRow = [];
 
-
-
 class Board {
-    static directions = [[0,1], [0,-1],[-1, 0], [1, 0], [1,1], [-1,1], [-1,-1], [1, -1]];
-        
-    constructor(height, width ){
-        this.height = height;
-        this.width = width;
-        this.initGameBoard();
-    }
-    initGameBoard(){
-        this.gameBoard = new Array(this.height).map((row) =>  new Array(this.width).map((cell) => false) );   
-    }
+  DIRECTIONS = [
+    [0, 1],
+    [0, -1],
+    [-1, 0],
+    [1, 0],
+    [1, 1],
+    [-1, 1],
+    [-1, -1],
+    [1, -1],
+  ];
 
-    toggleSquare(x, y){
-        this.gameBoard[x][y] = !this.gameBoard[x][y];
-    }
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+    this.initGameBoard();
+  }
 
-    setSquare(x, y, isAlive){
-        this.gameBoard[x][y] = isAlive;
-    }
+  initGameBoard() {
+    this.gameBoard = Array.from({ length: this.height }, () =>
+      Array.from({ length: this.width }, () => false)
+    );
+  }
 
-    countLiveCells(x, y){
-        return directions.reduce( (count, direction) => {
-            try{
-                return this.gameBoard[x + direction[0]][y + direction[1]] ? count++ : count;
-            }catch{
-                return count;
-            }
-        }, 0);
-    }
-    printBoard() {
-        this.gameBoard.forEach(row => {
-            row.forEach(cell => {
-                console.log(cell ? 'X' : '-' );
-            });
-            console.log("\n");
-        });
-    }
-};
+  toggleSquare(x, y) {
+    this.gameBoard[x][y] = !this.gameBoard[x][y];
+  }
+
+  setSquare(x, y, isAlive) {
+    this.gameBoard[x][y] = isAlive;
+  }
+
+  countLiveCells(x, y) {
+    return this.DIRECTIONS.reduce((count, direction) => {
+      try {
+        return this.gameBoard[y + direction[1]][x + direction[0]]
+          ? count + 1
+          : count;
+      } catch {
+        return count;
+      }
+    }, 0);
+  }
+
+  printBoard() {
+    this.gameBoard.forEach((row) => {
+      console.log(
+        row.reduce((prev, cell) => {
+          return cell ? `${prev}X` : `${prev}_`;
+        }, "")
+      );
+    });
+    console.log("\n");
+  }
+}
 
 let liveCellRules = (liveCellCount) => {
-    if(liveCellCount <= 1) return false;
-    if(liveCellCount <= 3) return true;
-    return false;
-}
+  if (liveCellCount <= 1) return false;
+  if (liveCellCount <= 3) return true;
+  return false;
+};
 
 let deadCellRules = (liveCellCount) => {
-    return liveCellCount === 3;
-} 
+  return liveCellCount === 3;
+};
 
 let willBeAlive = (isAlive, liveCellCount) => {
-    return isAlive ? liveCellRules(liveCellCount) : deadCellRules(liveCellCount);
-} 
+  return isAlive ? liveCellRules(liveCellCount) : deadCellRules(liveCellCount);
+};
 
 let processNextBoard = (board) => {
-    return board.gameBoard.map((row, rowIndex) => {
-        return row.map((cell, colIndex) => {
-            return willBeAlive(cell, board.countLiveCells(colIndex, rowIndex));
-        });
+  return board.gameBoard.map((row, rowIndex) => {
+    return row.map((cell, colIndex) => {
+      return willBeAlive(cell, board.countLiveCells(colIndex, rowIndex));
     });
-}
+  });
+};
 
-let board = new Board(100,100);
-board.toggleSquare(50, 50);
-let nextBoard = new Board(100, 100);
+let board = new Board(20, 20);
+board.toggleSquare(9, 9);
+board.toggleSquare(10, 9);
+board.toggleSquare(9, 10);
+board.toggleSquare(9, 11);
+
+let nextBoard = new Board(20, 20);
 let inProgress = true;
 board.printBoard();
 
-while(inProgress){
-    nextBoard.gameBoard = processNextBoard(board);
-    nextBoard.printBoard();
-    inProgress = false;
+let count = 0;
+while (inProgress) {
+  nextBoard.gameBoard = processNextBoard(board);
+  nextBoard.printBoard();
+  board = nextBoard;
+  count++;
+  if (count === 6) inProgress = false;
 }
-
 
 // 1. a live cell with zero or one live neighbours will die.
 // 1. a live cell with two or three live neighbours will remain alive
